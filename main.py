a1ed75f1ca05f32910109d4ecfb089171e16a304
Your Name <you@example.com>
2024-01-26 18:03:01 +0300

    dfs

--- main.py
+++ main.py
@@ -1,19 +1,45 @@
 from fake_useragent import UserAgent
-from clint.textui import progress
+from bs4 import BeautifulSoup
 import requests
+from random import randint
+from time import sleep
+
 ua = UserAgent()
 user_agent = ua.random
-url = 'https://promodj.com/download/7553204/7553204.mp3'
+links = []
+ids = []
 
-headers = {
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-         "User-Agent": user_agent
-         }
-r = requests.get(url=url, headers=headers, stream=True)
+# Забираем ссылки на скачивание с двух первых страниц
+def get_links():
+    for i in range(1, 3):
+        url = f'https://promodj.com/mixes?page={i}'        
+        r = requests.get(url=url)
+        soup = BeautifulSoup(r.text, "lxml")
+        download = soup.find_all(class_="player_standard_tool player_standard_tool__downloads")
+        for i in download:
+            url = i.get("href")
+            links.append(url)
+# скачиваем mp3
+def get_mp3():
+    for i in range(len(links)):
+        delay = randint(33,66)
+        print(f"спим {delay}")
+        sleep(delay)
+        headers = {
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+                "User-Agent": user_agent
+                }
+        ids = links[i].split('/')[4]
+        print(f'id - {ids}')
+        r = requests.get(url=links[i], headers=headers, stream=True)
+        print(f'download {links[i]}')
+        with open (f"{ids}.mp3", "wb") as f:
+            f.write(r.content)
+        
 
-with open("7553204.mp3", "wb") as file:
-    total_length = int(r.headers.get('content-length'))
-    for chunk in progress.bar(r.iter_content(chunk_size=1024), expected_size=(total_length/1024) + 1): 
-        if chunk:
-            file.write(chunk)
-            file.flush()
+def main():
+    get_links()
+    get_mp3()
+
+if __name__ == '__main__':
+    main()
